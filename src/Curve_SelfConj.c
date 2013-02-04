9b4e951994a3d76ac16187632535f4076957528c
danielbrake <ofloveandhate@users.noreply.github.com>
2013-02-04 20:30:06 +0000

    initial commit of source .c files.

---
+++ src/Curve_SelfConj.c
@@ -0,0 +1,54 @@
+#include "Curve_SelfConj.h"
+
+
+
+
+
+
+
+
+
+
+void computeCurveSelfConj(witness_set_d Wnew,
+						  vec_d pi,
+						  curveDecomp_d *C){
+	//IN DEVELOPMENT
+  
+  
+	// 1) check rank
+  
+  
+  // 2) randomize down to N-1 equations
+  
+  // 3) initalize data structure
+    // is this initialization is performed before this function?
+  
+  // 4) solve for critical conditions for random complex projection
+    // if no box, determine box
+  
+    // if have box, intersect box with C
+  
+  // 5) compute h_o, get critical points
+  
+  // 6) find points P
+    // T = \pi(crit)
+    // S = midpoints of T
+  
+    // for ( s \in S)
+      //find P
+  
+  
+  // 7) find edge endpoints
+  
+    // track left/right
+    // start at (x,t) = (p,1)
+    // call endpoint q.
+      //perform check on q \in crit, 
+  
+  //set C->V_0, C->V_1, C->E
+	
+	return;
+}
+
+
+
