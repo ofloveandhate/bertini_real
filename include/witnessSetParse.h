c9a9dfb5490d84c528c7d03d7aea5feea9bec468
danielbrake <danielbrake@265e88f5-ed14-43aa-8353-fef749e72172>
2013-02-04 20:27:33 +0000

    initial commit of header files.

---
+++ include/witnessSetParse.h
@@ -0,0 +1,10 @@
+#include "memory.h"
+#include "data_type.h"
+
+#ifndef _WITNESS_SET_PARSE
+#define _WITNESS_SET_PARSE
+
+int witnessSetParse(witness_set_d *W, char *witness_set_file, int num_vars);
+
+
+#endif
