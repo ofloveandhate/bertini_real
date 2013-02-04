caaf770aca77438e1838a2059c74b4f60a07f032
danielbrake <ofloveandhate@users.noreply.github.com>
2013-02-04 20:27:41 +0000

    initial commit of header files.

---
+++ programStartup.h
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <gmp.h>
+#include <time.h>
+#include <float.h>
+#include <limits.h>
+#include <mpfr.h>
+#include <mpf2mpfr.h>
+
+
+#include "memory.h"
+#include "data_type.h"
+
+#define BERTINI_REAL_VERSION_STRING "1.0"
+
+
+
+#ifndef _PROGRAM_STARTUP_H
+#define _PROGRAM_STARTUP_H
+
+int startup(int argC, char *args[], char **inputName, char **startName);
+
+int checkWritePrivilege();
+
+#endif
+
+
