f1c03195b5cbb53f4febf9ddd761f31cf5c34434
danielbrake <danielbrake@265e88f5-ed14-43aa-8353-fef749e72172>
2013-02-04 20:29:01 +0000

    initial commit of header files.

---
+++ include/bertini_real.h
@@ -0,0 +1,34 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <gmp.h>
+#include <time.h>
+#include <float.h>
+#include <limits.h>
+#include <mpfr.h>
+#include <mpf2mpfr.h>
+
+
+
+#include "checkSelfConjugate.h"
+#include "Curve_NotSelfConj.h"
+#include "Curve_SelfConj.h"
+#include "data_type.h"
+#include "isosingular.h"
+#include "memory.h"
+#include "programStartup.h"
+#include "witnessSetParse.h"
+
+
+#ifndef _BERTINI_REAL_H
+#define _BERTINI_REAL_H
+
+
+
+#endif
+
+
