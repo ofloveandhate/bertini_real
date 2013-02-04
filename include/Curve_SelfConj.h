c0d03c93061bb05ad38aa4f57a2c9fe122369d3b
danielbrake <ofloveandhate@users.noreply.github.com>
2013-02-04 20:28:37 +0000

    initial commit of header files.

---
+++ include/Curve_SelfConj.h
@@ -0,0 +1,23 @@
+#include "data_type.h"
+#include "memory.h"
+
+#ifndef _CURVE_SELFCONJ_H
+#define _CURVE_SELFCONJ_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <gmp.h>
+#include <time.h>
+#include <float.h>
+#include <limits.h>
+#include <mpfr.h>
+#include <mpf2mpfr.h>
+
+
+void computeCurveSelfConj(witness_set_d,vec_d,curveDecomp_d*);
+
+#endif
