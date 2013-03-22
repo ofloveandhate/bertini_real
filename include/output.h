cc2d1e277738fd4f69fc620d109f76c19102451a
danielbrake <danielbrake@265e88f5-ed14-43aa-8353-fef749e72172>
2013-03-22 21:08:49 +0000

    added header code

---
+++ include/output.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <gmp.h>
+#include <time.h>
+#include <float.h>
+#include <limits.h>
+#include <mpfr.h>
+#include <mpf2mpfr.h>
+
+
+#ifndef _BR_OUTPUT_H
+#define _BR_OUTPUT_H
+
+#include "cascade.h"
+#include "polysolve.h"  // the bertini  eval_funcs
+
+#include "fileops.h"
+#include "data_type.h"
+
+#include "partitionParse.h"
+
+
+
+
+#endif
+
+
