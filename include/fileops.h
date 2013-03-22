41eaeaef95fcec7868a18a7814c43462a8bd2a4f
danielbrake <danielbrake@265e88f5-ed14-43aa-8353-fef749e72172>
2013-03-22 21:07:57 +0000

    added header code

---
+++ include/fileops.h
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <gmp.h>
+#include <time.h>
+#include <float.h>
+#include <limits.h>
+#include <mpfr.h>
+#include <mpf2mpfr.h>
+
+#include <sys/stat.h>         /* declare the 'stat' structure */
+#include <sys/types.h>
+
+
+
+
+
+
+#ifndef _FILEOPS_H
+#define _FILEOPS_H
+
+
+#include "polysolve.h"
+
+
+
+
+FILE *safe_fopen_read(char * filename);
+
+FILE *safe_fopen_write(char * filename);
+
+FILE *safe_fopen_append(char * filename);
+
+
+#endif
+
