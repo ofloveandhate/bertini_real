3bd935c3177dc3bb828d8783494de3f5c6cccf3d
danielbrake <danielbrake@265e88f5-ed14-43aa-8353-fef749e72172>
2013-05-15 18:54:18 +0000

    rest of the commit.
    also, have multilin solver.

--- include/witnessSet.h
+++ include/witnessSet.h
@@ -3,15 +3,39 @@
 #include <gmp.h>
 #include <mpfr.h>
 
-#ifndef _WITNESS_SET_PARSE
-#define _WITNESS_SET_PARSE
+#ifndef WITNESS_SET_H_
+#define WITNESS_SET_H_
 
 #include "data_type.h"
 #include "polysolve.h"
 #include "fileops.h"
 
-void get_variable_names(witness_set *W);
 
 int witnessSetParse(witness_set *W, char *witness_set_file, int num_vars);
+
+
+
+void merge_witness_sets(witness_set *W_out,witness_set W_left,witness_set W_right);
+
+void init_variable_names(witness_set *W, int num_vars);
+
+void cp_names(witness_set *W_out, witness_set W_in);
+void cp_linears(witness_set *W_out, witness_set W_in);
+void cp_patches(witness_set *W_out, witness_set W_in);
+void cp_witness_set(witness_set *W_out, witness_set W_in);
+void init_witness_set_d(witness_set *W);
+void get_variable_names(witness_set *W);
+
+void clear_witness_set(witness_set W);
+void print_witness_set_to_screen(witness_set W);
+
+
+
+
+void write_homogeneous_coordinates(witness_set W, char filename[]);
+void write_dehomogenized_coordinates(witness_set W, char filename[]);
+void write_linears(witness_set W, char filename[]);
+
+
 
 #endif
