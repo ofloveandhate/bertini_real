6312bfc5415069362737e4331937ffd84aee8663
danielbrake <ofloveandhate@users.noreply.github.com>
2013-05-21 22:12:40 +0000

    added nullspace method source

---
+++ include/solverNullspace.h
@@ -0,0 +1,360 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <gmp.h>
+#include <time.h>
+#include <float.h>
+#include <limits.h>
+#include <mpfr.h>
+#include <mpf2mpfr.h>
+
+
+#ifndef SOLVER_NULLSPACE_H
+#define SOLVER_NULLSPACE_H
+
+#include "cascade.h"
+#include "polysolve.h"  // the bertini  eval_funcs
+
+#include "fileops.h"
+#include "data_type.h"
+
+#include "determinant_derivative.h"
+#include "programStartup.h"
+#include "postProcessing.h"
+#include "witnessSet.h"
+
+
+typedef struct
+{
+	int target_dim;
+	
+	int num_randomized_eqns;
+	int *jac_degrees;
+	vec_mp **starting_linears; // outer layer should have as many as there are randomized equations
+	// inside layer has number corresponding to randomized_degrees
+	
+	int num_v_linears;
+	vec_mp *v_linears;         // should be as many in here as there are randomized equations
+	
+	vec_mp v_patch;
+	
+	vec_mp *source_projection;
+	vec_mp *target_projection; // # of these should be target_dim (for now)
+	
+	mat_mp randomizer_matrix;
+} nullspace_config;
+
+
+
+
+
+
+// the mp version
+// this must be defined before the double version, because double has mp.
+typedef struct
+{
+	
+  patch_eval_data_mp patch;
+	
+  preproc_data preProcData;
+	
+	prog_t *SLP;
+	
+	mpq_t *gamma_rat;
+	comp_mp gamma;
+	
+	mat_mp randomizer_matrix;
+	mat_mp randomizer_matrix_full_prec;
+	
+	
+	int target_dim;
+	
+	int num_randomized_eqns;
+	int *jac_degrees;
+	
+	vec_mp **starting_linears; // outer layer should have as many as there are randomized equations
+	// inside layer has number corresponding to randomized_degrees
+	vec_mp **starting_linears_full_prec; // outer layer should have as many as there are randomized equations
+	// inside layer has number corresponding to randomized_degrees
+	
+	
+	int num_v_linears;
+	vec_mp *v_linears;         // should be as many in here as there are randomized equations
+	vec_mp *v_linears_full_prec;         // should be as many in here as there are randomized equations
+
+	vec_mp v_patch;
+	
+	vec_mp *source_projection;
+	vec_mp *target_projection; // # of these should be target_dim (for now)
+	
+	vec_mp *source_projection_full_prec;
+	vec_mp *target_projection_full_prec; // # of these should be target_dim (for now)
+	
+	int num_variables;
+	
+#ifdef printpathlinprod
+	FILE *FOUT;
+	int num_steps;
+#endif
+	
+	int curr_prec;
+} nullspacejac_eval_data_mp;
+
+
+
+typedef struct
+{
+	
+	nullspacejac_eval_data_mp *BED_mp; // used only for AMP
+	
+	
+  patch_eval_data_d patch;
+	preproc_data preProcData;
+	prog_t *SLP;
+	
+	comp_d gamma;
+	mat_d randomizer_matrix;
+	
+	int num_variables;
+	
+	
+	
+	int target_dim;
+	
+	int num_randomized_eqns;
+	int *jac_degrees;
+	
+	vec_d **starting_linears; // outer layer should have as many as there are randomized equations
+	// inside layer has number corresponding to randomized_degrees
+
+	
+	int num_v_linears;
+	vec_d *v_linears;         // should be as many in here as there are randomized equations
+	
+	vec_d v_patch;
+	
+	vec_d *source_projection;
+	vec_d *target_projection; // # of these should be target_dim (for now)
+	
+	
+	
+	
+	
+#ifdef printpathlinprod
+	FILE *FOUT;
+	int num_steps;
+#endif
+	
+	
+} nullspacejac_eval_data_d;
+
+
+
+
+
+
+
+/** the main function for finding critical conditions WRT a projection
+ */
+
+int nullspacejac_solver_main(int										MPType,
+														 witness_set						W, // carries with it the start points, and the linears.
+														 witness_set						*W_new, // new data goes in here
+														 nullspace_config				*ns_config,
+														 solver_configuration		*solve_options);
+
+
+
+int nullspacejac_solver_d(int											MPType, //, double parse_time, unsigned int currentSeed
+													witness_set							W,  // includes the initial linear.
+													witness_set							*W_new,
+													nullspace_config				*ns_config,
+													solver_configuration		*solve_options);
+
+
+
+void nullspacejac_track_d(trackingStats *trackCount,
+													FILE *OUT, FILE *RAWOUT, FILE *MIDOUT,
+													witness_set W,
+													post_process_t *endPoints,  // for holding the produced data.
+													FILE *FAIL,
+													int pathMod, tracker_config_t *T,
+													nullspacejac_eval_data_d *ED_d,
+													nullspacejac_eval_data_mp *ED_mp,
+													int (*eval_func_d)(point_d, point_d, vec_d, mat_d, mat_d, point_d, comp_d, void const *),
+													int (*eval_func_mp)(point_mp, point_mp, vec_mp, mat_mp, mat_mp, point_mp, comp_mp, void const *),
+													int (*change_prec)(void const *, int),
+													int (*find_dehom)(point_d, point_mp, int *, point_d, point_mp, int, void const *, void const *),
+													solver_configuration *solve_options);
+
+void nullspacejac_track_path_d(int pathNum, endgame_data_t *EG_out,
+															 point_data_d *Pin,
+															 FILE *OUT, FILE *MIDOUT, tracker_config_t *T,
+															 void const *ED_d, void const *ED_mp,
+															 int (*eval_func_d)(point_d, point_d, vec_d, mat_d, mat_d, point_d, comp_d, void const *),
+															 int (*eval_func_mp)(point_mp, point_mp, vec_mp, mat_mp, mat_mp, point_mp, comp_mp, void const *),
+															 int (*change_prec)(void const *, int),
+															 int (*find_dehom)(point_d, point_mp, int *, point_d, point_mp, int, void const *, void const *));
+
+
+
+int nullspacejac_setup_d(FILE **OUT, char *outName,
+												 FILE **midOUT, char *midName,
+												 tracker_config_t *T,
+												 nullspacejac_eval_data_d *ED,
+												 prog_t *dummyProg,
+												 int **startSub, int **endSub, int **startFunc, int **endFunc, int **startJvsub, int **endJvsub, int **startJv, int **endJv, int ***subFuncsBelow,
+												 int (**eval_d)(point_d, point_d, vec_d, mat_d, mat_d, point_d, comp_d, void const *),
+												 int (**eval_mp)(point_mp, point_mp, vec_mp, mat_mp, mat_mp, point_mp, comp_mp, void const *),
+												 char *preprocFile, char *degreeFile,
+												 int findStartPts, char *pointsIN, char *pointsOUT,
+												 witness_set W,
+												 nullspace_config				*ns_config,
+												 solver_configuration *solve_options);
+
+//the new custom evaluator for this solver
+
+int nullspacejac_eval_d(point_d funcVals, point_d parVals, vec_d parDer, mat_d Jv, mat_d Jp, point_d current_variable_values, comp_d pathVars, void const *ED);
+
+
+
+
+void printnullspacejacRelevantData(nullspacejac_eval_data_d *ED_d, nullspacejac_eval_data_mp *ED_mp, int MPType, int eqbyeqMethod, FILE *FP);
+
+
+void nullspacejac_eval_clear_d(nullspacejac_eval_data_d *ED, int clearRegen, int MPType);
+
+
+
+void setup_nullspacejac_omp_d(int max_threads, endgame_data_t **EG, trackingStats **trackCount_copy, trackingStats *trackCount,
+															FILE ***OUT_copy, FILE *OUT, FILE ***RAWOUT_copy, FILE *RAWOUT,
+															FILE ***MIDOUT_copy, FILE *MIDOUT, FILE ***FAIL_copy, FILE *FAIL,
+															FILE ***NONSOLN_copy, FILE *NONSOLN,
+															tracker_config_t **T_copy, tracker_config_t *T,
+															nullspacejac_eval_data_d **BED_copy, nullspacejac_eval_data_d *ED_d, nullspacejac_eval_data_mp *ED_mp);
+
+
+void clear_nullspacejac_omp_d(int max_threads, endgame_data_t **EG, trackingStats **trackCount_copy, trackingStats *trackCount, FILE ***OUT_copy, FILE *OUT, FILE ***RAWOUT_copy, FILE *RAWOUT, FILE ***MIDOUT_copy, FILE *MIDOUT, FILE ***FAIL_copy, FILE *FAIL, FILE ***NONSOLN_copy, FILE *NONSOLN, tracker_config_t **T_copy, nullspacejac_eval_data_d **BED_copy);
+
+
+
+void setupnullspacejacEval_d(tracker_config_t *T,char preprocFile[], char degreeFile[], prog_t *dummyProg,
+														 int squareSize, int patchType, int ssType, int MPType,
+														 void const *ptr1, void const *ptr2, void const *ptr3, void const *ptr4,// what are these supposed to point to?
+														 nullspacejac_eval_data_d *BED, int adjustDegrees,
+														 witness_set W,
+														 nullspace_config *ns_config,
+														 solver_configuration *solve_options);
+
+
+
+void start_system_eval_data_clear_d(start_system_eval_data_d *SSED);//actually lives in bertini library...  testing if this works.
+
+void patch_eval_data_clear_d(patch_eval_data_d *PED);//another which lives in bertini
+void patch_eval_data_clear_mp(patch_eval_data_mp *PED);//another which lives in bertini
+void changePatchPrec_mp(int new_prec, patch_eval_data_mp *PED); // in bertini
+
+
+void cp_nullspacejac_eval_data_d(nullspacejac_eval_data_d *BED, nullspacejac_eval_data_d *BED_d_input, nullspacejac_eval_data_mp *BED_mp_input, int MPType);
+
+
+int nullspacejac_dehom(point_d out_d, point_mp out_mp, int *out_prec, point_d in_d, point_mp in_mp, int in_prec, void const *ED_d, void const *ED_mp);
+
+
+
+int change_nullspacejac_eval_prec(void const *ED, int prec);
+void change_nullspacejac_eval_prec_mp(int new_prec, nullspacejac_eval_data_mp *BED);
+
+int nullspacejac_solver_mp(int MPType, //, double parse_time, unsigned int currentSeed
+													 witness_set W,  // includes the initial linear.
+													 witness_set *W_new,
+													 nullspace_config *ns_config,
+													 solver_configuration *solve_options);
+
+
+
+void nullspacejac_track_mp(trackingStats *trackCount,
+													 FILE *OUT, FILE *RAWOUT, FILE *MIDOUT,
+													 witness_set W,
+													 post_process_t *endPoints,
+													 FILE *FAIL,
+													 int pathMod, tracker_config_t *T,
+													 nullspacejac_eval_data_mp *ED,
+													 int (*eval_func_mp)(point_mp, point_mp, vec_mp, mat_mp, mat_mp, point_mp, comp_mp, void const *),
+													 int (*change_prec)(void const *, int),
+													 int (*find_dehom)(point_d, point_mp, int *, point_d, point_mp, int, void const *, void const *),
+													 solver_configuration *solve_options);
+
+
+void nullspacejac_track_path_mp(int pathNum, endgame_data_t *EG_out,
+																point_data_mp *Pin,
+																FILE *OUT, FILE *MIDOUT, tracker_config_t *T,
+																void const *ED,
+																int (*eval_func_mp)(point_mp, point_mp, vec_mp, mat_mp, mat_mp, point_mp, comp_mp, void const *),
+																int (*change_prec)(void const *, int),
+																int (*find_dehom)(point_d, point_mp, int *, point_d, point_mp, int, void const *, void const *));
+
+
+int nullspacejac_setup_mp(FILE **OUT, char *outName,
+													FILE **midOUT, char *midName,
+													tracker_config_t *T,
+													nullspacejac_eval_data_mp *ED,
+													prog_t *dummyProg,
+													int **startSub, int **endSub, int **startFunc, int **endFunc, int **startJvsub, int **endJvsub, int **startJv, int **endJv, int ***subFuncsBelow,
+													int (**eval_d)(point_d, point_d, vec_d, mat_d, mat_d, point_d, comp_d, void const *),
+													int (**eval_mp)(point_mp, point_mp, vec_mp, mat_mp, mat_mp, point_mp, comp_mp, void const *),
+													char *preprocFile, char *degreeFile,
+													int findStartPts, char *pointsIN, char *pointsOUT,
+													witness_set W,
+													nullspace_config				*ns_config,
+													solver_configuration *solve_options);
+
+
+int nullspacejac_eval_mp(point_mp funcVals, point_mp parVals, vec_mp parDer, mat_mp Jv, mat_mp Jp, point_mp current_variable_values, comp_mp pathVars, void const *ED);
+
+
+
+void nullspacejac_eval_clear_mp(nullspacejac_eval_data_mp *ED, int clearRegen, int MPType);
+
+
+void setup_nullspacejac_omp_mp(int max_threads, endgame_data_t **EG, trackingStats **trackCount_copy, trackingStats *trackCount,
+															 FILE ***OUT_copy, FILE *OUT, FILE ***RAWOUT_copy, FILE *RAWOUT,
+															 FILE ***MIDOUT_copy, FILE *MIDOUT, FILE ***FAIL_copy, FILE *FAIL,
+															 FILE ***NONSOLN_copy, FILE *NONSOLN,
+															 tracker_config_t **T_copy, tracker_config_t *T,
+															 nullspacejac_eval_data_mp **BED_copy, nullspacejac_eval_data_mp *ED_mp);
+
+
+void clear_nullspacejac_omp_mp(int max_threads, endgame_data_t **EG, trackingStats **trackCount_copy, trackingStats *trackCount, FILE ***OUT_copy, FILE *OUT, FILE ***RAWOUT_copy, FILE *RAWOUT, FILE ***MIDOUT_copy, FILE *MIDOUT, FILE ***FAIL_copy, FILE *FAIL, FILE ***NONSOLN_copy, FILE *NONSOLN, tracker_config_t **T_copy, nullspacejac_eval_data_mp **BED_copy);
+
+
+void setupnullspacejacEval_mp(char preprocFile[], char degreeFile[], prog_t *dummyProg,
+															int squareSize, int patchType, int ssType, int prec,
+															void const *ptr1, void const *ptr2, void const *ptr3, void const *ptr4,
+															nullspacejac_eval_data_mp *BED, int adjustDegrees,
+															witness_set W,
+															nullspace_config				*ns_config,
+															solver_configuration *solve_options);
+
+void cp_nullspacejac_eval_data_mp(nullspacejac_eval_data_mp *BED, nullspacejac_eval_data_mp *BED_mp_input, int MPType);
+
+
+
+int check_issoln_nullspacejac_d(endgame_data_t *EG,
+																tracker_config_t *T,
+																void const *ED);
+int check_issoln_nullspacejac_mp(endgame_data_t *EG,
+																 tracker_config_t *T,
+																 void const *ED);
+
+
+void check_nullspace_evaluator(point_mp current_values,
+															 void const *ED);
+
+
+
+#endif
+
+
