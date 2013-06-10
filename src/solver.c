6ac3364ac69f001280573d1d3025c0b2749c5876
danielbrake <danielbrake@265e88f5-ed14-43aa-8353-fef749e72172>
2013-06-10 18:52:16 +0000

    added generic solver source file.

---
+++ src/solver.c
@@ -0,0 +1,110 @@
+#include "solver.h"
+
+
+/**
+ sets the start_pts structure to hold all points in W
+ 
+ \param startPts the value being set.  should be NULL input.
+ \param W the witness_set input
+ 
+ */
+void generic_set_start_pts_d(point_data_d * startPts,
+													 witness_set W)
+{
+	int ii; // counters
+	
+	startPts = (point_data_d *)bmalloc(W.num_pts * sizeof(point_data_d));
+	
+	for (ii = 0; ii < W.num_pts; ii++)
+	{ // setup startPts[ii]
+		init_point_data_d(&startPts[ii], W.num_variables); // also performs initialization on the point inside startPts
+		change_size_vec_d(startPts[ii].point,W.num_variables);
+		startPts[ii].point->size = W.num_variables;
+		
+		//1 set the coordinates
+		vec_mp_to_d(startPts[ii].point, W.pts_mp[ii]);
+		
+		//2 set the start time to 1.
+		set_one_d(startPts[ii].time);
+	}
+}
+
+
+
+void generic_track_path_d(int pathNum, endgame_data_t *EG_out,
+																	 point_data_d *Pin,
+																	 FILE *OUT, FILE *MIDOUT, tracker_config_t *T,
+																	 void const *ED_d, void const *ED_mp,
+																	 int (*eval_func_d)(point_d, point_d, vec_d, mat_d, mat_d, point_d, comp_d, void const *),
+																	 int (*eval_func_mp)(point_mp, point_mp, vec_mp, mat_mp, mat_mp, point_mp, comp_mp, void const *),
+																	 int (*change_prec)(void const *, int),
+																	 int (*find_dehom)(point_d, point_mp, int *, point_d, point_mp, int, void const *, void const *))
+/***************************************************************\
+ * USAGE:                                                        *
+ * ARGUMENTS:                                                    *
+ * RETURN VALUES:                                                *
+ * NOTES: actually does the zero-dimensional tracking and sets   *
+ *  up EG_out                                                    *
+ \***************************************************************/
+{
+	
+	
+	
+	EG_out->pathNum = pathNum;
+  EG_out->codim = 0; // this is ignored
+	
+  T->first_step_of_path = 1;
+	
+  if (T->MPType == 2)
+  { // track using AMP
+    EG_out->prec = EG_out->last_approx_prec = 52;
+		
+		EG_out->retVal = endgame_amp(T->endgameNumber, EG_out->pathNum, &EG_out->prec, &EG_out->first_increase, &EG_out->PD_d, &EG_out->PD_mp, &EG_out->last_approx_prec, EG_out->last_approx_d, EG_out->last_approx_mp, Pin, T, OUT, MIDOUT, ED_d, ED_mp, eval_func_d, eval_func_mp, change_prec, find_dehom);
+		
+    if (EG_out->prec == 52)
+    { // copy over values in double precision
+      EG_out->latest_newton_residual_d = T->latest_newton_residual_d;
+      EG_out->t_val_at_latest_sample_point_d = T->t_val_at_latest_sample_point;
+      EG_out->error_at_latest_sample_point_d = T->error_at_latest_sample_point;
+      findFunctionResidual_conditionNumber_d(&EG_out->function_residual_d, &EG_out->condition_number, &EG_out->PD_d, ED_d, eval_func_d);
+    }
+    else
+    { // make sure that the other MP things are set to the correct precision
+      mpf_clear(EG_out->function_residual_mp);
+      mpf_init2(EG_out->function_residual_mp, EG_out->prec);
+			
+      mpf_clear(EG_out->latest_newton_residual_mp);
+      mpf_init2(EG_out->latest_newton_residual_mp, EG_out->prec);
+			
+      mpf_clear(EG_out->t_val_at_latest_sample_point_mp);
+      mpf_init2(EG_out->t_val_at_latest_sample_point_mp, EG_out->prec);
+			
+      mpf_clear(EG_out->error_at_latest_sample_point_mp);
+      mpf_init2(EG_out->error_at_latest_sample_point_mp, EG_out->prec);
+			
+      // copy over the values
+      mpf_set(EG_out->latest_newton_residual_mp, T->latest_newton_residual_mp);
+      mpf_set_d(EG_out->t_val_at_latest_sample_point_mp, T->t_val_at_latest_sample_point);
+      mpf_set_d(EG_out->error_at_latest_sample_point_mp, T->error_at_latest_sample_point);
+      findFunctionResidual_conditionNumber_mp(EG_out->function_residual_mp, &EG_out->condition_number, &EG_out->PD_mp, ED_mp, eval_func_mp);
+    }
+  }
+  else
+  { // track using double precision
+    EG_out->prec = EG_out->last_approx_prec = 52;
+		
+    EG_out->retVal = endgame_d(T->endgameNumber, EG_out->pathNum, &EG_out->PD_d, EG_out->last_approx_d, Pin, T, OUT, MIDOUT, ED_d, eval_func_d, find_dehom);  // WHERE THE ACTUAL TRACKING HAPPENS
+    EG_out->first_increase = 0;
+    // copy over values in double precision
+    EG_out->latest_newton_residual_d = T->latest_newton_residual_d;
+    EG_out->t_val_at_latest_sample_point_d = T->t_val_at_latest_sample_point;
+    EG_out->error_at_latest_sample_point_d = T->error_at_latest_sample_point;
+    findFunctionResidual_conditionNumber_d(&EG_out->function_residual_d, &EG_out->condition_number, &EG_out->PD_d, ED_d, eval_func_d);
+  }
+	
+  return;
+}
+
+
+
+
