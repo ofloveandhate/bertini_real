de4b4b6b0f1cda58aa293df6689fd9e140f54202
danielbrake <ofloveandhate@users.noreply.github.com>
2013-07-23 17:16:11 +0000

    added derivative_systems header

---
+++ include/derivative_systems.hpp
@@ -0,0 +1,60 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <gmp.h>
+#include <time.h>
+#include <float.h>
+#include <limits.h>
+#include <mpfr.h>
+#include <mpf2mpfr.h>
+
+#include <vector>
+#include <map>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <cmath>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+
+
+#ifndef DERIVATIVE_SYSTEMS_H
+#define DERIVATIVE_SYSTEMS_H
+
+
+extern "C" {
+#include "polysolve.h"
+}
+
+#include "programConfiguration.hpp"
+#include "data_type.hpp"
+
+#include "fileops.hpp"
+#include "missing_bertini_headers.hpp"
+
+
+
+void addItems(int *numItems, char ***itemNames, int **itemLines, FILE *IN, int lineNumber);
+
+void parse_names(int *numItems, char ***itemNames, int **itemLines, FILE *IN, char *name, int num_declarations);
+
+std::string just_constants(boost::filesystem::path filename,
+																int numConstants, char **consts, int *lineConstants);
+
+
+void write_matrix_as_constants(mat_mp M, std::string prefix, FILE *OUT);
+
+void write_vector_as_constants(vec_mp V, std::string prefix, FILE *OUT);
+
+
+#endif
