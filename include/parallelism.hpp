36a040349290eff41b203c39c94d39337648475c
danielbrake <danielbrake@265e88f5-ed14-43aa-8353-fef749e72172>
2013-07-31 18:48:00 +0000

    added parallelism header code.

---
+++ include/parallelism.hpp
@@ -0,0 +1,40 @@
+
+#ifndef BR_PARALLELISM_H
+#define BR_PARALLELISM_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <gmp.h>
+#include <time.h>
+#include <float.h>
+#include <limits.h>
+#include <mpfr.h>
+#include <mpf2mpfr.h>
+#include <sstream>
+#include <iostream>
+
+
+
+
+
+extern "C" {
+#include "cascade.h"
+}
+extern "C" {
+#include "polysolve.h"
+}
+
+
+
+
+
+
+#endif
+
+
+
+
+
