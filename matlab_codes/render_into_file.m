281885a1b29b80aad45636768bfb65fe918b40f0
danielbrake <ofloveandhate@users.noreply.github.com>
2014-02-14 16:29:56 +0000

    updated render_into_file

--- matlab_codes/render_into_file.m
+++ matlab_codes/render_into_file.m
@@ -5,8 +5,7 @@
 %
 % input:
 %  plotmode -- a string which forms the basis of a filename
-%  dataname -- a string ending with .mat which further specifies the
-%                filename
+
 %  plot_params -- a structure with data members for setting up the save.
 %      members:  fontsize, an integer
 %                window, a 4-element vector in standard matlab format for
@@ -27,49 +26,56 @@
 % danielthebrake@gmail.com
 
 
-function plot_params = render_into_file(basename, plot_params)
+function plot_params = render_into_file(varargin)
 %
 
-if nargin<2
-	fontsize = 16;
-	window = get(gcf,'Position');
-	format = 'eps';
-	format_flag = 'epsc2';
+
+if isempty(varargin)
+	
+	plot_params.fontsize = 16;
+	plot_params.window = get(gcf,'Position');
+	plot_params.format = 'eps';
+	plot_params.format_flag = 'epsc2';
+	plot_params.basename = 'default_filename';
+elseif and(ischar(varargin{1} ), length(varargin)==1)
+	
+	plot_params.fontsize = 16;
+	plot_params.window = get(gcf,'Position');
+	plot_params.format = 'eps';
+	plot_params.format_flag = 'epsc2';
+	
+	plot_params.basename = varargin{1};
+	
+elseif isstruct(varargin{1})
+	plot_params = varargin{1};
 else
-	fontsize  = plot_params.fontsize;
-	window   = plot_params.window;
-	format   = plot_params.format;
-	format_flag   = plot_params.format_flag;
+	plot_params.fontsize = 16;
+	plot_params.window = get(gcf,'Position');
+	plot_params.format = 'eps';
+	plot_params.format_flag = 'epsc2';
+	plot_params.basename = 'default_filename';
 end
 
 
-if nargin < 1
-	basename = 'default_filename';
-end
 
 fig1 = gcf;
 
-set(fig1,'Position',window,'PaperPositionMode','auto');
-set(gca,'FontSize',fontsize-2);
+set(fig1,'PaperPositionMode','auto');
+set(gca,'FontSize',plot_params.fontsize-2);
+
+% labelText('FontName','Times New Roman');
 
 % basename = sprintf('%s_%s',plotmode,dataname(1:end-4));
 
-currname = increment_name(basename);
-nameforfile = sprintf('%s.%s',currname,format);
-print(fig1,nameforfile,sprintf('-d%s',format_flag));
-% 
-% if input('save another view of this plot? \n if so, set the window, and press 1 then enter.');
-% 	plot_params.fontsize = fontsize;
-% 	plot_params.window = window;
-% 	plot_params.format = format;
-% 	plot_params.format_flag = format_flag;
-% 	
-% 	render_into_file(basename,plot_params);
-% 	
-% end
-plot_params.fontsize = fontsize;
-plot_params.window = window;
-plot_params.format = format;
-plot_params.format_flag = format_flag;
+currname = increment_name(plot_params.basename);
+nameforfile = sprintf('%s.%s',currname,plot_params.format);
 
+evalme = sprintf('print(fig1,''%s'',''-d%s''',nameforfile,plot_params.format_flag);
+for ii = 2:length(varargin)
+    evalme = sprintf('%s,''%s''',evalme,varargin{ii});
 end
+evalme = sprintf('%s);',evalme);
+evalme
+eval(evalme);
+end
+
