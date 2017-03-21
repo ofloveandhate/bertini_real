a8f887d371e158d9089b8bde1eac75f7c37b2924
Christopher Lembo <clembo@nd.edu>
2017-03-21 14:01:55 -0400

    Changed Color to Magenta
    In sampler.cpp, I changed line 383: "adaptive by movement not implemented for surfaces, using adaptive by distance" from red to magenta

--- src/sampler/sampler.cpp
+++ src/sampler/sampler.cpp
@@ -380,7 +380,7 @@ int main(int argC, char *args[])
 					break;
 				}
 				case sampler_configuration::Mode::AdaptivePredMovement:
-					std::cout << color::red() << "adaptive by movement not implemented for surfaces, using adaptive by distance" << color::console_default() << "\n\n";
+					std::cout << color:: magenta() << "adaptive by movement not implemented for surfaces, using adaptive by distance" << color::console_default() << "\n\n";
 				case sampler_configuration::Mode::AdaptiveConsecDistance:
 				{
 					surf_input.AdaptiveSampler(V,
