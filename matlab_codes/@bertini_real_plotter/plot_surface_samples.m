e369fe3ea25ff8e94575c84f141d013490aaaf25
dani brake <danielthebrake@gmail.com>
2017-03-23 13:14:49 -0400

    added missing wrapper if on selected faces if surface was not sampled

--- matlab_codes/@bertini_real_plotter/plot_surface_samples.m
+++ matlab_codes/@bertini_real_plotter/plot_surface_samples.m
@@ -80,9 +80,12 @@ function sampler_faces = plot_surf_samples_multicolor(br_plotter)
 colors = br_plotter.options.colormap(length(br_plotter.BRinfo.sampler_data));
 
 total_num_faces = 0;
+
+if ~isempty(br_plotter.BRinfo.sampler_data)
 for cc = 1:length(br_plotter.options.which_faces)
 	ii = br_plotter.options.which_faces(cc);
 	total_num_faces = total_num_faces+size(br_plotter.BRinfo.sampler_data{ii},1);
+end
 end
 
 sampler_faces = zeros(total_num_faces,3);
