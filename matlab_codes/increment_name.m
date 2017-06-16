e8d43c6dbaf4b6b1806b317ae46bdfb6037270b4
dani brake <danielthebrake@gmail.com>
2017-06-16 15:10:47 -0400

    updated render_into_file to be more programmatic, and detect extensions, etc

--- matlab_codes/increment_name.m
+++ matlab_codes/increment_name.m
@@ -5,10 +5,13 @@
 %names.
 
 % daniel brake
-% colorado state university, north carolina state university, notre dame
+% colorado state university, north carolina state university, notre dame, 
+% university of wisconsin eau claire
 % mathematics and applied mathematics
-% 2013-14
+% 2013-17
+%
 % danielthebrake@gmail.com
+% brakeda@uwec.edu
 
 
 
@@ -18,12 +21,7 @@ file_list = dir([basename '_*']);
 if ~isempty(file_list)
 	filenumbers = zeros(1,length(file_list));
 	for ii = 1:length(file_list)
-		potential_number = str2double(file_list(ii).name(length(basename)+2:end-4));
-		if isnan(potential_number)
-			potential_number = -1;
-		end
-		
-		filenumbers(ii) = potential_number;%subtract 4 to remove extension
+		filenumbers(ii) = extract_number(file_list(ii).name, basename);
 	end
 else
 	filenumbers = 0;
@@ -33,3 +31,31 @@ newname = sprintf('%s_%i',basename,max(filenumbers)+1);
 
 
 end
+
+function n = extract_number(name, basename)
+	
+if has_extension(name)
+	r = [length(basename)+2 length(name)-4];
+else
+	r = [length(basename)+2 length(name)];
+end
+n = str2double(name(r(1):r(2)));
+
+if isnan(n)
+	n = -1;
+end
+
+
+end
+
+
+function result =  has_extension(name)
+
+if strcmp((name(end-3)), '.')
+	result = true;
+else
+	result = false;
+end
+
+end
+
--- matlab_codes/render_into_file.m
+++ matlab_codes/render_into_file.m
@@ -1,11 +1,19 @@
 % render_into_file(basename, plot_params)
 %
 % a utility for saving plots to a file from matlab.
-% intended for use with the paramotopy suite of functions.
+%
+% intended for use with the 
+%   - paramotopy, 
+%   - bertini_real, 
+%   - bertini_tropical, 
+%   - bertini_real-chebfun 
+% suites of functions.
+%
+% if the argument is `gendef`, you will get the current default values.
+%
 %
 % input:
 %  plotmode -- a string which forms the basis of a filename
-
 %  plot_params -- a structure with data members for setting up the save.
 %      members:  format, a string for the format to save.  see the print
 %                         help.
@@ -15,27 +23,30 @@
 %
 %
 %
-
-% daniel brake
-% colorado state, north carolina state, notre dame
-% mathematics
-% 2013-15
+% dani brake
+% colorado state university , 
+% north carolina state university, 
+% university of notre dame, 
+% university of wisconsin eau claire
+%
+% mathematics, applied mathematics
+% 2013-17
 % danielthebrake@gmail.com
 
 
 function plot_params = render_into_file(varargin)
-%
 
-[~, default_filename, ~] = fileparts(pwd);
-default_filename = default_filename(~isspace(default_filename));
-			
-display(varargin)
-if isempty(varargin)
+if isa(varargin{1},'char')
+	if strcmp(varargin,'gendef')
+		plot_params = generate_defaults();
+		return
+	end
+end
 	
-	plot_params.fontsize = 16;
-	plot_params.format = 'eps';
-	plot_params.format_flag = 'psc2';
-	plot_params.basename = default_filename;
+
+if isempty(varargin)
+	plot_params = generate_defaults();
+	
 elseif and(ischar(varargin{1} ), length(varargin)==1)
 	
 	plot_params.format = 'eps';
@@ -49,37 +60,102 @@ elseif isstruct(varargin{1})
 	if ~isfield(plot_params,'basename')
 		error('incomplete plot_params.  add field ''basename''');
 	end
+	
+	if ~isfield(plot_params,'resolution')
+		error('incomplete plot_params.  add field ''resolution''');
+	end
+	
+	args = varargin(2:end);
 else
-	plot_params.window = get(gcf,'Position');
-	plot_params.format = 'eps';
-	plot_params.format_flag = 'psc2';
-	plot_params.basename = default_filename;
+	plot_params = generate_defaults();
 end
 
-fig1 = gcf;
+plot_params.fig = gcf;
 
-set(fig1,'PaperPositionMode','auto');
+PaperPositionCrap(plot_params);
 
-if isfield(fig1,'PaperSize')
-	fig1.PaperSize = fig1.PaperPosition(3:4);
+evalme = get_command(plot_params, args);
+
+% display(evalme);
+eval(evalme);
+display(sprintf('image saved as %s',get_final_name(plot_params)));
 end
 
 
 
-currname = increment_name(plot_params.basename);
-nameforfile = sprintf('%s.%s',currname,plot_params.format);
+function evalme = get_command(plot_params, args)
 
-varargin
+final_name = get_final_name(plot_params);
+
 if strcmp(plot_params.format,'eps')
-	varargin = [varargin, {'-fillpage'}]
+	args = [args, {'-fillpage'}];
 end
 
-evalme = sprintf('print(fig1,''%s'',''-d%s''',nameforfile,plot_params.format_flag);
-for ii = 2:length(varargin)
-    evalme = sprintf('%s,''%s''',evalme,varargin{ii});
+evalme = sprintf('print(plot_params.fig,''%s'',''-d%s'', ''-r%i''',final_name,plot_params.format_flag, plot_params.resolution);
+for ii = 1:length(args)
+    evalme = sprintf('%s,''%s''',evalme,args{ii});
 end
 evalme = sprintf('%s);',evalme);
-display(evalme);
-eval(evalme);
+
+end
+
+
+function final_name = get_final_name(plot_params)
+
+name_no_ext = get_name(plot_params);
+final_name = sprintf('%s.%s',name_no_ext,plot_params.format);
+
+end
+
+
+
+
+function name_no_ext = get_name(plot_params)
+
+if ~isfield(plot_params,'autoname')
+	name_no_ext = increment_name(plot_params.basename);
+else
+	if plot_params.autoname
+		name_no_ext = increment_name(plot_params.basename);
+	else
+		name_no_ext = plot_params.basename;
+	end
+end
+
+end
+
+
+% i truly hate this function, but matlab does weird sizing things,
+% inconsistently with version, for eps and pdf prints.  
+% please help me deal with this better, if you can.
+function PaperPositionCrap(plot_params)
+
+set(plot_params.fig,'PaperPositionMode','auto');
+
+if isfield(plot_params.fig,'PaperSize')
+	plot_params.fig.PaperSize = fig.PaperPosition(3:4);
+end
+
+end
+
+
+
+function plot_params = generate_defaults()
+
+plot_params.window = get(gcf,'Position');
+plot_params.basename = default_filename();
+plot_params.format = 'eps';
+plot_params.format_flag = 'deps';
+plot_params.autoname = true;
+plot_params.resolution = 150;
+end
+
+
+
+function name = default_filename()
+
+[~, name, ~] = fileparts(pwd);
+name = name(~isspace(name));
+
 end
 
