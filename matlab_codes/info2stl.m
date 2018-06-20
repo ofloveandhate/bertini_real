3ed880e75b693417d40921aad09b0892694c8bb3
danielle amethyst <brakeda@uwec.edu>
2018-06-20 13:38:10 -0500

    added info2stl
    taking in BRinfo and automatically saving the faces as stl.  solves issue #46

---
+++ matlab_codes/info2stl.m
@@ -0,0 +1,40 @@
+% [] = info2stl(BRinfo, options)
+%
+% a function for extracting the faces and vertices from
+% a BRinfo object, and saving the result as an stl
+% possibly for 3d printing, or whatever; i don't get to tell
+% you what to do with your stl's
+% 
+% 
+
+function [] = info2stl(BRinfo, varargin)
+
+
+options = process_options(BRinfo, varargin);
+
+fv = info2fv(BRinfo, options.which_faces);
+fv2stl(fv,options);
+
+
+end % the function
+
+
+
+function options = process_options(BRinfo, varargin)
+
+[options.containing, options.filename, ~] = fileparts(pwd);
+
+options.which_faces = [];
+if isempty(options.which_faces)
+	options.which_faces = 1:BRinfo.num_faces;
+elseif max(options.which_faces) > BRinfo.num_faces
+	error('trying to plot faces which don''t exist. requested index %i > num faces %i',...
+		max(options.which_faces),...
+		BRinfo.num_faces);
+end
+				
+
+options.remove_duplicates = 0;
+options.align = 0;
+options.save_mat = 0;
+end
