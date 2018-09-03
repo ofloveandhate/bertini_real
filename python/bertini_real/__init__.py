f3b6897fb2d24f5bf3b4ddb50108870ef7f6048d
danielle brake <danielleamethystbrake@gmail.com>
2018-09-03 16:10:53 -0400

    adjustments so can actually plot (vertices on) surfaces

--- python/bertini_real/__init__.py
+++ python/bertini_real/__init__.py
@@ -3,27 +3,29 @@ import bertini_real.brdata
 import bertini_real.surface
 import bertini_real.curve
 import bertini_real.util
-
-
+import bertini_real.plot
 
 
 def gather():
 
-    a = util.next_filenumber()
+	a = util.next_filenumber()
 
-    fileName = "BRdata" + str(a) + ".pkl"
-    fileObject = open(fileName,'wb')
+	fileName = "BRdata" + str(a) + ".pkl"
+	fileObject = open(fileName,'wb')
 #file parsing
-    b = brdata.BRData()
+	b = brdata.BRData()
 
-    print("saving to file " + fileName)
+	print("saving to file " + fileName)
 
-    import dill
-    #b is written to fileObject through dump function
-    dill.dump(b,fileObject)
-    #closes the file
-    fileObject.close()
+	import dill
+	#b is written to fileObject through dump function
+	dill.dump(b,fileObject)
+	#closes the file
+	fileObject.close()
+
 
 def gather_and_plot():
 	gather()
-	bertini_real.Plot.plot()
+	return bertini_real.plot.plot()
+
+
--- python/bertini_real/plot/__init__.py
+++ python/bertini_real/plot/__init__.py
@@ -5,147 +5,179 @@
 # Danielle Brake
 # Fall 2018
 
-import bertini_real as br
-
-
-
-
-
 import os
-from BRdata import BRdata
-from surface import surface, Curve
-import Util
+from bertini_real.brdata import BRData
+from bertini_real.surface import Surface, Curve
+import bertini_real.util
 import dill
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+
+
+class Options(object):
+	def __init__(self):
+		self.option1 = "value1"
+
+
+
 class Plotter(object):
 
-    def __init__(self, data = []):
-        self.decomposition = data
 
+	def __init__(self, data = None, options = Options()):
 
-    def plot(self):
-        print("plotting object of dimension " + str(self.decomposition.dimension))
-        self.fig = plt.figure()
+		if data is None:
+			self.ReadMostRecent()
+		else:
+			self.decomposition = data
 
-        if self.decomposition.num_variables==2:
-            self.ax = self.fig.add_subplot(111)
-        else:
-            self.ax = self.fig.add_subplot(111, projection='3d')
-            self.ax.set_zlabel("z")
-        self.PlotVertices()
-
-        if self.decomposition.dimension == 1:
-            self.PlotCurve(self.decomposition)
-        elif self.decomposition.dimension == 2:
-            self.Plotsurface(self.decomposition)
-
-        plt.xlabel("x")
-        plt.ylabel("y")
-        #plt.xlabel
-        plt.title(os.getcwd().split(os.sep)[-1])
-        plt.show()
+		self.options = options
 
 
 
+	def plot(self):
 
-    def ReadMostRecent(self):
-        filenum = util.highest_filenumber()
+		print("plotting object of dimension " + str(self.decomposition.dimension))
+		
+		self.make_figure()
+		self.make_axes()
+			
 
-        fileName = "BRdata" + str(filenum) + ".pkl"
+		# this should be conditionally done based on some flag
+		self.PlotVertices()
 
-        print("reading from file " + fileName)
+		if self.decomposition.dimension == 1:
+			self.PlotCurve(self.decomposition)
+		elif self.decomposition.dimension == 2:
+			self.Plotsurface(self.decomposition)
 
-        fileObject = open(fileName,'rb')
-        self.decomposition = dill.load(fileObject)
-        fileObject.close()
-
-    def PlotVertices(self):
-
-        xs = []
-        ys = []
-        zs = []
-
-        for v in self.decomposition.vertices:
-            xs.append(v['point'][0].real)
-            ys.append(v['point'][1].real)
-
-            if self.decomposition.num_variables>2:
-                zs.append(v['point'][2].real)
+		self.label_axes()
+		self.apply_title()
 
 
-        if self.decomposition.num_variables==2:
-            self.ax.scatter(xs, ys)#v['point'][
-        else:
-            self.ax.scatter(xs, ys, zs, zdir='z', s=20, c=None, depthshade=True)#v['point'][
-
-    def PlotCurve(self, curve):
-        num_nondegen_edges=0
-        nondegen=[]
-        for i in range(curve.curve.num_edges):
-            e=curve.curve.edges[i]
-            if e[0]!=e[1]!=e[2]:
-                num_nondegen_edges=num_nondegen_edges+1
-                nondegen.append(i)
-
-        colormap = plt.cm.inferno
-        color_list=([colormap(i) for i in np.linspace(0, 1,num_nondegen_edges)])
-
-        for i in range(num_nondegen_edges):
-            color=color_list[i]
-            self.PlotEdge(curve,nondegen[i],color)
-
-    def PlotEdge(self, curve, edge_index,color):
-
-        xs=[]
-        ys=[]
-        zs=[]
-
-        inds = self.decomposition.curve.sampler_data[edge_index]
-        for i in inds:
-            v = self.decomposition.vertices[i]
-            xs.append(v['point'][0].real)
-            ys.append(v['point'][1].real)
-            if self.decomposition.num_variables>2:
-                zs.append(v['point'][2].real)
-
-        if self.decomposition.num_variables==2:
-            self.ax.plot(xs, ys, c=color)#v['point'][
-        else:
-            self.ax.plot(xs, ys, zs, zdir='z', c=color)#v['point']
-
-
-    def Plotsurface(self, surf):
-        print("Plotsurface unimplemented yet")
+		plt.show()
 
 
 
 
-
-def plot():
-
-    p = br.plot.Plotter()
-    #p takes the action ReadMostRecent()
-    p.ReadMostRecent()
-    #member functions have to be called as a member function
-    p.plot()
-    return p
+	def make_figure(self):
+		self.fig = plt.figure()
 
 
 
 
-def plot(data):
-    b = BRplotter(data)
-    b.plot();
-    return b
+	def make_axes(self):
+		if self.decomposition.num_variables==2:
+			self.ax = self.fig.add_subplot(111)
+		else:
+			self.ax = self.fig.add_subplot(111, projection='3d')
 
 
 
-if __name__ == "__main__":
-     b = BRplotter()
-     b.ReadMostRecent()
-     b.plot()
+	def apply_title(self):
+		plt.title(os.getcwd().split(os.sep)[-1])
+
+
+
+	def label_axes(self):
+		#todo: these should be set from the decomposition, not assumed to be x,y,z
+		self.ax.set_xlabel("x")
+		self.ax.set_ylabel("y")
+		if self.decomposition.dimension is 2:
+			self.ax.set_zlabel("z")
+
+		
+
+
+
+
+	def ReadMostRecent(self):
+		filenum = bertini_real.util.highest_filenumber()
+
+		fileName = "BRdata" + str(filenum) + ".pkl"
+
+		print("reading from file " + fileName)
+
+		fileObject = open(fileName,'rb')
+		self.decomposition = dill.load(fileObject)
+		fileObject.close()
+
+
+	'''
+	renders all vertices
+
+	todo: make them colored based on a function
+	'''
+	def PlotVertices(self):
+
+		xs = []
+		ys = []
+		zs = []
+
+		for v in self.decomposition.vertices:
+			xs.append(v['point'][0].real)
+			ys.append(v['point'][1].real)
+
+			if self.decomposition.num_variables>2:
+				zs.append(v['point'][2].real)
+
+
+		if self.decomposition.num_variables==2:
+			self.ax.scatter(xs, ys)#v['point'][
+		else:
+			self.ax.scatter(xs, ys, zs, zdir='z', s=5, c=None, depthshade=True)#v['point'][
+
+	def PlotCurve(self, curve):
+		num_nondegen_edges=0
+		nondegen=[]
+		for i in range(curve.curve.num_edges):
+			e=curve.curve.edges[i]
+			if e[0]!=e[1]!=e[2]:
+				num_nondegen_edges=num_nondegen_edges+1
+				nondegen.append(i)
+
+		colormap = plt.cm.inferno
+		color_list=([colormap(i) for i in np.linspace(0, 1,num_nondegen_edges)])
+
+		for i in range(num_nondegen_edges):
+			color=color_list[i]
+			self.PlotEdge(curve,nondegen[i],color)
+
+	def PlotEdge(self, curve, edge_index,color):
+
+		xs=[]
+		ys=[]
+		zs=[]
+
+		inds = self.decomposition.curve.sampler_data[edge_index]
+		for i in inds:
+			v = self.decomposition.vertices[i]
+			xs.append(v['point'][0].real)
+			ys.append(v['point'][1].real)
+			if self.decomposition.num_variables>2:
+				zs.append(v['point'][2].real)
+
+		if self.decomposition.num_variables==2:
+			self.ax.plot(xs, ys, c=color)#v['point'][
+		else:
+			self.ax.plot(xs, ys, zs, zdir='z', c=color)#v['point']
+
+
+	def Plotsurface(self, surf):
+		print("Plotsurface unimplemented yet.")
+
+
+
+
+
+
+def plot(data = None):
+
+	b = Plotter(data)
+	b.plot();
+	return b
+
+
 
 
