a0413a13cf06016a846693bb587fc3da793df61f
danielle brake <danielleamethystbrake@gmail.com>
2018-09-07 11:30:25 -0400

    fixed bug with not getting the correct field

--- matlab_codes/@bertini_real_plotter/change_edge_width.m
+++ matlab_codes/@bertini_real_plotter/change_edge_width.m
@@ -16,7 +16,7 @@ end
 new_val = alpha_options.all;
 
 if br_plotter.options.edge_width ~= new_val
-	set(br_plotter.handles.faces(:),'LineWidth',new_val); 
+	set(br_plotter.handles.faces.raw,'LineWidth',new_val); 
 	set(br_plotter.handles.faces.samples,'LineWidth',new_val);
 	br_plotter.options.edge_width = new_val;
 end
