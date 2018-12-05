ed4a6eb7b5ebd8c28e2db6c70534c58e2e8bce95
Dan <hessler.dan@gmail.com>
2018-12-05 09:32:18 -0600

    working, surface-agnostic, glumpyplotter object. plots using opengl. lots of work to do still

---
+++ python/bertini_real/glumpyplotter/__init__.py
@@ -0,0 +1,136 @@
+"""
+Dan Hessler
+University of Wisconsin, Eau Claire
+Fall 2018
+Porting to Glumpy for faster surface rendering
+using OpenGL
+"""
+import numpy as np
+from glumpy import app, gl, glm, gloo
+import bertini_real
+
+
+class GlumpyPlotter(object):
+    """ creates the glumpyplotter object """
+    def __init__(self, data=None):
+        if data is None:
+            self.decomposition = bertini_real.data.ReadMostRecent()
+        else:
+            self.decomposition = data
+
+
+    def plot(self):
+        """ method used to plot a surface """
+        print("Plotting object of dimension: {}".format(self.decomposition.dimension))
+
+        data = bertini_real.data.ReadMostRecent()
+        tuples = data.surface.surface_sampler_data
+
+        def extract_points(data):
+            """Extract points from vertices"""
+            points = []
+
+            for vertice in data.vertices:
+                """ we use 3 here because a triangle consists
+                of three points """
+                point = [None]*3
+
+                for j in range(3):
+                    point[j] = vertice['point'][j].real
+                points.append(point)
+            return points
+
+        points = extract_points(data)
+
+        triangle = []
+
+        for i in range(len(tuples)):
+
+            for tri in tuples[i]:
+                f = int(tri[0])
+                s = int(tri[1])
+                t = int(tri[2])
+
+                k = [f, s, t]
+                triangle.append(k)
+
+        triangle = np.asarray(triangle)
+
+        vertex = """
+        uniform mat4   u_model;         // Model matrix
+        uniform mat4   u_view;          // View matrix
+        uniform mat4   u_projection;    // Projection matrix
+        attribute vec3 a_position;      // Vertex position
+        void main()
+        {
+            gl_Position = u_projection * u_view * u_model * vec4(a_position,1.0);
+        }
+        """
+
+        fragment = """
+        void main()
+        {
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+        """
+
+        window = app.Window(width=1024, height=1024,
+                            color=(0.30, 0.30, 0.35, 1.00))
+
+
+        verts = np.zeros(len(points), [("a_position", np.float32, 3)])
+        verts["a_position"] = points
+
+
+        verts = verts.view(gloo.VertexBuffer)
+        indeces = np.array(triangle).astype(np.uint32)
+        indeces = indeces.view(gloo.IndexBuffer)
+
+        surface = gloo.Program(vertex, fragment)
+        surface.bind(verts)
+        surface['u_model'] = np.eye(4, dtype=np.float32)
+        surface['u_view'] = glm.translation(0, 0, -5)
+        phi, theta = 40, 30
+
+        @window.event
+        def on_draw(draw_triangles):
+            """ draws and rotates the surface """
+            global phi, theta
+            window.clear()
+
+            #  surface.draw(gl.GL_TRIANGLES, indeces)
+            surface.draw(gl.GL_LINES, indeces)
+
+            # TODO
+            # fix the scope of phi, and theta so we can do rotations
+
+            #  Make surface rotate
+            #  theta += 0.5 # degrees
+            #  phi += 0.5 # degrees
+            theta = 40
+            phi = 30
+            model = np.eye(4, dtype=np.float32)
+            glm.rotate(model, theta, 0, 0, 1)
+            glm.rotate(model, phi, 0, 1, 0)
+            surface['u_model'] = model
+
+
+        @window.event
+        def on_resize(width, height):
+            """ deals with resizing the image """
+            surface['u_projection'] = glm.perspective(45.0, width / float(height), 2.0, 100.0)
+
+        @window.event
+        def on_init():
+            """ initializes opengl? """
+            gl.glEnable(gl.GL_DEPTH_TEST)
+
+
+        app.run()
+
+def plot(data=None):
+    """ simply calls the plot method """
+
+    surface = GlumpyPlotter(data)
+    surface.plot()
+    
