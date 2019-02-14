f7dfbaf61679747edddc7fc4d4f32cce23504167
Foong Min Wong <foongmin@hotmail.com>
2019-02-14 16:33:21 -0600

    remove fvtostl from the codes

--- python/bertini_real/glumpyplotter/__init__.py
+++ python/bertini_real/glumpyplotter/__init__.py
@@ -5,11 +5,6 @@ Fall 2018 - Spring 2019
 Porting to Glumpy for faster surface rendering
 using OpenGL
 
-Foong Min Wong
-University of Wisconsin, Eau Claire
-Fall 2018 - Spring 2019
-Implementing stereolithography export feature
-
 Current Version:
     Surface agnostic
     Mouse (trackball) implementation
@@ -31,13 +26,10 @@ TODO:
               not looking too promising....
 """
 
-import os
 import numpy as np
 from glumpy import app, gl, glm, gloo
 from glumpy.transforms import Trackball, Position
 import bertini_real
-from stl import mesh
-import trimesh
 
 class GlumpyPlotter():
     """ creates the glumpyplotter object """
@@ -46,66 +38,6 @@ class GlumpyPlotter():
             self.decomposition = bertini_real.data.ReadMostRecent()
         else:
             self.decomposition = data
-
-    def fvtostl(self):
-
-        def extract_points(data):
-            """Extract points from vertices"""
-
-            points = []
-
-            for vertex in data.vertices:
-                """ we use 3 here because a triangle consists
-                of three points """
-                point = [None]*3
-
-                for j in range(3):
-                    point[j] = vertex['point'][j].real
-                points.append(point)
-            return points
-
-        data = self.decomposition
-        points = extract_points(data)
-        faces = self.decomposition.surface.surface_sampler_data
-       
-        # add vertex and surface to mesh
-        vertex = []
-        
-        for p in points:
-            vertex.append(p)
-
-        vertex_np_array = np.array(vertex)
-
-        face = []
-
-        for f in faces: # for each face
-            for tri in f: # for triangle in face
-                face.append([tri[0],tri[1],tri[2]])
-
-        face_np_array = np.array(face)
- 
-        obj = mesh.Mesh(np.zeros(face_np_array.shape[0], dtype=mesh.Mesh.dtype))
-
-        for i, f in enumerate(face_np_array):
-            for j in range(3):
-                obj.vectors[i][j] = vertex_np_array[f[j],:]
-
-        # get object filename
-        fileName = os.getcwd().split(os.sep)[-1]
-
-        obj.save('a' + fileName + '.stl')
-
-        normmesh = trimesh.load_mesh('a' + fileName + '.stl')
-
-        normmesh.fix_normals()
-
-        for facet in normmesh.facets:
-            normmesh.visual.face_colors[facet] = trimesh.visual.random_color()
-
-        # normmesh.show()
-        
-        normmesh.export(file_obj='anorm' + fileName + '.stl', file_type='stl')
-        print('Export STL successfully!')
 
     def plot(self, color_function):
         """ method used to plot a surface """
@@ -254,7 +186,3 @@ def plot(color_function=None, data=None):
 
     surface = GlumpyPlotter(data)
     surface.plot(color_function)
-
-def fvtostl(data=None):
-    surface = GlumpyPlotter(data)
-    surface.fvtostl()
