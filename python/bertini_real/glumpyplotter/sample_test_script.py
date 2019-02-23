fe61c7cf597e2dbb96d83cd18075536492827a7c
Dan <hessler.dan@gmail.com>
2019-02-23 08:19:26 -0600

    updated test script for current version

--- python/bertini_real/glumpyplotter/sample_test_script.py
+++ python/bertini_real/glumpyplotter/sample_test_script.py
@@ -1,31 +1,41 @@
-""" test script to compare matplotlib plotter to glumpyplotter """
 import bertini_real
 
-#  bertini_real.plot.plot()
+def function(x,y,z):
+    return x + y + z
 
-# color functions
-def f1(x,y,z):
-    return x**2 + y
+fn = function
 
-def f2(x,y,z):
-    return y**2 + z
+bertini_real.glumpyplotter.plot()
+#  bertini_real.glumpyplotter.plot(cmap='jet')
+#  bertini_real.glumpyplotter.plot(cmap='inferno', color_function=fn)
 
-def f3(x,y,z):
-    return z**2 + x
 
-function = f1, f2, f3
 
-bertini_real.glumpyplotter.plot(function)
 
-"""
-these are just hardcoded examples of other color functions
-"""
 
-#  r = x**2 + y**2 + z**2
-#  g = x + y**2 + z**2
-#  b = x + y + z**2
 
-# this is my fav
-#  r = x**2
-#  g = y**2
-#  b = z**2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
