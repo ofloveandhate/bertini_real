fe3bcd6e024353ebf70dbccfa5662b614c807eb5
Dan <hessler.dan@gmail.com>
2019-03-13 13:42:32 -0500

    updated test script for use with critical curves

--- python/bertini_real/glumpyplotter/sample_test_script.py
+++ python/bertini_real/glumpyplotter/sample_test_script.py
@@ -8,3 +8,4 @@ fn = function
 bertini_real.glumpyplotter.plot()
 #  bertini_real.glumpyplotter.plot(cmap='jet')
 #  bertini_real.glumpyplotter.plot(cmap='inferno', color_function=fn)
+#  bertini_real.glumpyplotter.plot(cmap='inferno', color_function=fn, critical_curve=True)
