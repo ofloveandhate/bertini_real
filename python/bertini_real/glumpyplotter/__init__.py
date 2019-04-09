951068860acbd88ec70d3a405a1d92b15d9f3bd8
Dan <hessler.dan@gmail.com>
2019-04-08 19:37:35 -0500

    refactored plot() into plot_surface_samples() and plot_critical_curve()

--- python/bertini_real/glumpyplotter/__init__.py
+++ python/bertini_real/glumpyplotter/__init__.py
@@ -15,6 +15,33 @@ from glumpy import app, gl, glm, gloo
 from glumpy.transforms import Trackball, Position
 import bertini_real
 
+# ----------------------------------------------------------------------------- #
+#                    Vertex and fragment are OpenGL code
+#                             Used by Glumpy
+# ----------------------------------------------------------------------------- #
+
+vertex = """
+attribute vec4 a_color;         // Vertex Color
+uniform mat4   u_model;         // Model matrix
+uniform mat4   u_view;          // View matrix
+uniform mat4   u_projection;    // Projection matrix
+attribute vec3 position;        // Vertex position
+varying vec4   v_color;         // Interpolated fragment color (out)
+void main()
+{
+    v_color = a_color;
+    gl_Position = <transform>;
+}
+"""
+
+fragment = """
+varying vec4  v_color;          // Interpolated fragment color (in)
+void main()
+{
+    gl_FragColor = v_color;
+}
+"""
+
 class GlumpyPlotter():
     """ Creates the glumpyplotter object which is used to render
         3d surfaces created from bertini_real
@@ -31,81 +58,36 @@ class GlumpyPlotter():
         else:
             self.decomposition = data
 
-    def plot(self, cmap=None, color_function=None, critical_curve=False):
+    def plot_surface_samples(self, cmap=None, color_function=None):
         """ Method used to plot a surface. """
+
         print("Plotting object of dimension: {}".format(self.decomposition.dimension))
         data = self.decomposition
-        # TODO add functionality for:
-        #  critical point slices
-        #  midpoint slices
-        #  singlular curves
-        #  sphere curve
+        sampler_data = data.surface.surface_sampler_data
+        points = extract_points(data)
 
-        if critical_curve:
+        triangle = np.zeros(len(sampler_data))
+        for i in range(len(sampler_data)):
+            for tri in sampler_data[i]:
+                index_1 = int(tri[0])
+                index_2 = int(tri[1])
+                index_3 = int(tri[2])
 
-            tuples = data.surface.critical_curve.sampler_data
-            points = extract_curve_points(data)
-            triangle = tuples
-
-        else:   # just plots the surface samples
-
-            tuples = data.surface.surface_sampler_data
-            points = extract_points(data)
-
-            triangle = []
-            for i in range(len(tuples)):
-                for tri in tuples[i]:
-                    index_1 = int(tri[0])
-                    index_2 = int(tri[1])
-                    index_3 = int(tri[2])
-
-                    triple = [index_1, index_2, index_3]
-                    triangle.append(triple)
-
-        triangle = np.asarray(triangle)
-
-
-# ----------------------------------------------------------------------------- #
-#                    Vertex and fragment are OpenGL code
-# ----------------------------------------------------------------------------- #
-
-        vertex = """
-        attribute vec4 a_color;         // Vertex Color
-        uniform mat4   u_model;         // Model matrix
-        uniform mat4   u_view;          // View matrix
-        uniform mat4   u_projection;    // Projection matrix
-        attribute vec3 position;        // Vertex position
-        varying vec4   v_color;         // Interpolated fragment color (out)
-        void main()
-        {
-            v_color = a_color;
-            gl_Position = <transform>;
-        }
-        """
-
-        fragment = """
-        varying vec4  v_color;          // Interpolated fragment color (in)
-        void main()
-        {
-            gl_FragColor = v_color;
-        }
-        """
+                triple = [index_1, index_2, index_3]
+                triangle = np.append(triangle,triple)
 
         window = app.Window(width=2048, height=2048,
                             color=(0.30, 0.30, 0.35, 1.00))
 
-        # DO THESE 4 LINES FOR EACH EDGE OF CRITICAL CURVE
         verts = np.zeros(len(points), [("position", np.float32, 3),
                                        ("a_color", np.float32, 4)])
         verts["position"] = points
         verts["a_color"] = make_colors(verts["position"], cmap, color_function)
 
         verts = verts.view(gloo.VertexBuffer)
-        # stop looper here
 
-        if critical_curve is False:
-            indices = np.array(triangle).astype(np.uint32)
-            indices = indices.view(gloo.IndexBuffer)
+        indices = np.array(triangle).astype(np.uint32)
+        indices = indices.view(gloo.IndexBuffer)
 
         surface = gloo.Program(vertex, fragment)
         surface.bind(verts)
@@ -117,13 +99,7 @@ class GlumpyPlotter():
         @window.event
         def on_draw(draw_triangles):
             window.clear()
-
-            if critical_curve:
-                surface.draw(gl.GL_LINE_STRIP, triangle)
-            else:
-                surface.draw(gl.GL_TRIANGLES, indices)
-
-
+            surface.draw(gl.GL_TRIANGLES, indices)
 
         @window.event
         def on_init():
@@ -134,9 +110,54 @@ class GlumpyPlotter():
 
         app.run()
 
-# ----------------------------------------------------------------------------- #
+    def plot_critical_curve(self, cmap=None, color_function=None):
+        """ Method used to plot a surface's critical curve. """
 
-def plot(data=None, cmap='hsv', color_function=None, critical_curve=False):
+        print("Plotting object of dimension: {}".format(self.decomposition.dimension))
+        data = self.decomposition
+
+        points = extract_curve_points(data)
+        crit_curve = data.surface.surface_sampler_data
+        crit_curve = np.asarray(crit_curve)
+
+        #  print('====================Curve==================')
+        #  print(curve[0])
+        #  print('====================Points==================')
+        #  print(points)
+
+        window = app.Window(width=2048, height=2048,
+                            color=(0.30, 0.30, 0.35, 1.00))
+
+        surface = gloo.Program(vertex, fragment)
+        for edge in points:
+            verts = np.zeros(len(edge), [("position", np.float32, 3),
+                                         ("a_color", np.float32, 4)])
+            verts["position"] = edge
+            verts["a_color"] = make_colors(verts["position"], cmap, color_function)
+            verts = verts.view(gloo.VertexBuffer)
+
+            surface.bind(verts)
+
+        surface['u_model'] = np.eye(4, dtype=np.float32)
+        surface['u_view'] = glm.translation(0, 0, -5)
+        surface['transform'] = Trackball(Position("position"), znear=0)
+        window.attach(surface['transform'])
+
+        @window.event
+        def on_draw(draw_triangles):
+            window.clear()
+            surface.draw(gl.GL_LINE_STRIP, crit_curve)
+
+        @window.event
+        def on_init():
+            gl.glEnable(gl.GL_DEPTH_TEST)
+            gl.glPolygonOffset(1, 1)
+            gl.glEnable(gl.GL_LINE_SMOOTH)
+            gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+
+        app.run()
+
+def plot_surface_samples(data=None, cmap='hsv', color_function=None):
     """
     Sets default values for colormap if none are specified.
     """
@@ -145,7 +166,18 @@ def plot(data=None, cmap='hsv', color_function=None, critical_curve=False):
     cmap = plt.get_cmap(cmap)
 
     surface = GlumpyPlotter(data)
-    surface.plot(cmap, color_function, critical_curve)
+    surface.plot_surface_samples(cmap, color_function)
+
+def plot_critical_curve(data=None, cmap='hsv', color_function=None):
+    """
+    Sets default values for colormap if none are specified.
+    """
+    import matplotlib.pyplot as plt
+
+    cmap = plt.get_cmap(cmap)
+
+    surface = GlumpyPlotter(data)
+    surface.plot_critical_curve(cmap, color_function)
 
 # ----------------------------------------------------------------------------- #
 #                               Helper Methods
@@ -223,13 +255,21 @@ def extract_points(data):
 
 def extract_curve_points(data):
     points = []
+    all_points = []
+    curve = []
     for edge in data.surface.critical_curve.sampler_data:
         if len(edge) <= 1:
             continue
         for vertex in edge:
-            point = [None]*3
-            for i in range(3):
-                point[i] = data.vertices[vertex]['point'][i].real
+            point = (data.vertices[vertex]['point'][0].real,
+                     data.vertices[vertex]['point'][1].real,
+                     data.vertices[vertex]['point'][2].real)
             points.append(point)
+            all_points.append(point)
+        curve.append(points)
+        points = []
 
-    return points
+    # possibly fill with NaN's to show separators
+
+    # return curve, all_points
+    return curve
