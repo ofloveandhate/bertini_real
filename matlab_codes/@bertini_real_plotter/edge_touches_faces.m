d47eb34a8dc2f783ed435cfc38c8e4cc4267ca71
danielle brake <danielleamethystbrake@gmail.com>
2019-10-14 21:36:53 -0400

    fix bug in detecting edges touching a face

--- matlab_codes/@bertini_real_plotter/edge_touches_faces.m
+++ matlab_codes/@bertini_real_plotter/edge_touches_faces.m
@@ -8,27 +8,28 @@ function val = edge_touches_faces(br_plotter, edge_index, curve)
 	for ii = 1:length(br_plotter.options.which_faces)
 		face_ind = br_plotter.options.which_faces(ii);
 
-		this_face_touches = false;
-
 		
 		f = br_plotter.BRinfo.faces(face_ind); % unpack the current face
 		
 		
 		if m == f.midpoint 
-			this_face_touches = true;
+			val = true;
+			return
 		end
 
 		left_critslice = br_plotter.BRinfo.critpoint_slices{f.midslice_index};
 		for jj = 1:f.num_left
 			if m == left_critslice.edges(f.left(jj),2)
-				this_face_touches = true;
+				val = true;
+				return
 			end
 		end
 
 		right_critslice = br_plotter.BRinfo.critpoint_slices{f.midslice_index+1};
 		for jj = 1:f.num_right
 			if m == right_critslice.edges(f.right(jj),2)
-				this_face_touches = true;
+				val = true;
+				return
 			end
 		end
 	
@@ -36,21 +37,17 @@ function val = edge_touches_faces(br_plotter, edge_index, curve)
 		
 		curve_top = curve_with_inputname(br_plotter.BRinfo, f.system_top);
 		if m == curve_top.edges(f.top,2)
-			this_face_touches = true;
+			val = true;
+			return
 		end
 		
 		curve_bottom = curve_with_inputname(br_plotter.BRinfo, f.system_bottom);
 		if m == curve_bottom.edges(f.bottom,2)
-			this_face_touches = true;
+			val = true;
+			return
 		end
 		
-		if this_face_touches
-			val = this_face_touches;
-			break;
-		end
 	end
-
-
 
 end
 
