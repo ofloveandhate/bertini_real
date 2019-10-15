1e42fdee56b6ecb8af91c5627aabba88bb49d62a
danielle brake <danielleamethystbrake@gmail.com>
2019-10-14 21:40:50 -0400

    omg docs, plus a bugfix since `_to_` is often `2` in matlab

--- matlab_codes/save_all_fvs.m
+++ matlab_codes/save_all_fvs.m
@@ -1,10 +1,22 @@
+% [] = save_all_fvs(fvs,root_filename)
+%
+% saves a cell array of faces-vertices structs to stl's.
+%
+% requires two arguments.  
+%
+% fvs is a cell array of fv
+% root_filename is a string
+
+
 function save_all_fvs(fvs,root_filename)
 
 options.remove_duplicates = false;
 options.align = false;
+options.save_mat = false;
+
 for ii = 1:length(fvs)
 	options.filename = sprintf('%s_%i.stl',root_filename, ii);
-	fv_to_stl(fvs{ii}, options);
+	fv2stl(fvs{ii}, options);
 end
 
 end
