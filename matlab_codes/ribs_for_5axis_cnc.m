6cd68cae317358dafb886ca0f86989b1f7ca9ae5
danielle brake <danielleamethystbrake@gmail.com>
2019-10-20 18:49:13 -0400

    major jump forward in toolpath generation from ribs
    includes rotation, translation, visualization, and file writing.  nailed it!

--- matlab_codes/ribs_for_5axis_cnc.m
+++ matlab_codes/ribs_for_5axis_cnc.m
@@ -10,7 +10,10 @@
 % * `render` -- a boolean, indicates whether we'll render the paths to the
 %			screen.  default is true.
 %
-% * `normals_mode`, `normals` -- modes: `outward_from_origin`;
+% * 'normals_mode', 'normals' -- modes: 'outward_from_origin', 'none';
+%
+% this list of options may be incomplete because i'm lazy.  see `parse_options`
+% in this .m file.s
 %
 % Danielle Amethyst, 2019
 
@@ -28,6 +31,7 @@ for ii = 1:length(options.which_faces)
 	ribs{ii} = generate_zigzag(BRinfo,face_index,J,options);
 end
 
+
 if options.render
 	visualize_ribs(ribs,options);
 end
@@ -37,16 +41,9 @@ write_ribs(ribs,options);
 end
 
 
-function output = generate_zigzag(BRinfo, face_index, J, options)
+function toolpath = generate_zigzag(BRinfo, face_index, J, options)
 
-switch options.normals_mode
-	case 'outward_from_origin'
-		alignment_function = @align_normal_outward;
-	case 'none'
-		alignment_function = @(n,x) n;
-	otherwise
-		error('invalid choice of normals_mode');
-end
+
 
 ribs = BRinfo.ribs{face_index}; % unpack
 
@@ -56,7 +53,10 @@ for ii = 1:num_ribs
 	rib_sizes = length(ribs{ii});
 end
 
-output = zeros(6,sum(rib_sizes)); % preallocate
+alignment_function = make_alignment_handle(options); % ooh, meta
+affine_transform = make_affine_transform_handle(options);
+
+toolpath = zeros(6,sum(rib_sizes)); % preallocate
 
 c = 1;
 parity = 1;
@@ -70,9 +70,12 @@ for ii = 1:num_ribs
 	end
 	for jj = 1:length(r)
 		x = real(BRinfo.vertices(r(jj)).point(1:3));
-		n = compute_normal_vector(x,J,alignment_function);
+		n = compute_normal_vector(x,J);
+
+		[x,n] = affine_transform(x,n);
+		n = alignment_function(n,x);
 		
-		output(:,c) = [x;n];
+		toolpath(:,c) = [x;n];
 		c = c+1;
 	end
 end
@@ -88,16 +91,16 @@ end
 
 end
 
-function n = compute_normal_vector(x,jac, alignment_function)
+function n = compute_normal_vector(x,jac)
 
 nullspace = null(jac(x));
 n = cross(nullspace(:,1),nullspace(:,2));
-n = alignment_function(n,x);
 end
 
 
 function [] = visualize_ribs(ribs,options)
-hold off
+hold_cached = ishold;
+
 for ii = 1:length(ribs)
 	r = ribs{ii};
 	x = r(1,:);
@@ -110,21 +113,122 @@ for ii = 1:length(ribs)
 	quiver3(x,y,z,u,v,w);
 	hold on
 	plot3(x,y,z);
+	hold on
 end
 
 daspect([1 1 1 ]);
+
+if hold_cached
+	hold on
+else
+	hold off
+end
+
+
 end
 
 function write_ribs(ribs,options)
 
 base_name = options.base_filename;
 
+fprintf('writing files to files named `%s_*`\n',base_name);
+
 for ii = 1:length(options.which_faces)
-	face_index = options.which_faces(ii);
 	fid = fopen(sprintf('%s_%i',base_name,ii),'w');
-	
+	write_rib(fid,ribs{ii});
 	fclose(fid);
 end
+
+end
+
+
+function write_rib(fid,rib)
+[~,n] = size(rib);
+fprintf(fid,'%i\n',n);
+for ii = 1:n
+	fprintf(fid,'%1.16f ',rib(:,ii));
+	fprintf(fid,'\n');
+end
+end
+
+function [affine_transf] = make_affine_transform_handle(options)
+
+if options.rotate.flag
+	R = make_rotation_matrix(options.rotate.axis,options.rotate.angle);
+	r = @(x,n) rotate_xn(x,n,R);
+else
+	r =  @parrot_2;
+end
+
+
+if options.translate.flag
+	t = @(x,n) translate_xn(x,n,options.translate.vector);
+else
+	t = @parrot_2;
+end
+
+if options.rotate_first
+	first = r;
+	second = t;
+else
+	first = t;
+	second = r;
+end
+
+affine_transf = @(x,n) double_func_comp(x,n,first,second);
+
+end
+
+function [x,n] = double_func_comp(x,n,first,second)
+[x,n] = first(x,n);
+[x,n] = second(x,n);
+end
+
+function [x,n] = translate_xn(x,n,t)
+x = x+t;
+end
+
+% see also
+% https://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
+function [x,n] = rotate_xn(x,n,R)
+x = R*x;
+n = R*n;
+end
+
+
+function R = make_rotation_matrix(axis,angle)
+
+x = axis(1);
+y = axis(2);
+z = axis(3);
+
+c = cos(angle);
+s = sin(angle);
+R = [c+x^2*(1-c),    x*y*(1-c)-z*s,  x*z*(1-c)+y*s;...
+	 y*x*(1-c)+z*s,  c+y^2*(1-c),    y*z*(1-c)-x*s;...
+	 z*x*(1-c)-y*s,  z*y*(1-c)+x*s,  c+z^2*(1-c)];
+end
+
+
+function alignment_function = make_alignment_handle(options)
+
+switch options.normals_mode
+	case 'outward_from_origin'
+		alignment_function = @align_normal_outward;
+	case 'none'
+		alignment_function = @(n,x) n;
+	otherwise
+		error('invalid choice of normals_mode');
+end
+
+
+end
+
+
+
+
+% the un-function.  aaaraaarp!
+function [a,b] = parrot_2(a,b,varargin)
 
 end
 
@@ -133,6 +237,10 @@ function options = parse_options(BRinfo,varargin)
 
 options = default_options(BRinfo);
 options.base_filename = 'toolpaths';
+options.translate.flag = false;
+options.rotate.flag = false;
+options.rotate_first = true;
+options.affine_precedes_ribbing = true;
 
 if mod(length(varargin),2)~=0
 	error('options must come in pairs -- name, argument.  you have a mismatch');
@@ -148,6 +256,17 @@ while opt_ind < length(varargin)
 			options.render = varargin{arg_ind};
 		case {'normals','normals_mode'}
 			options.normals_mode = varargin{arg_ind};
+		case {'translate','translate_by'}
+			options.translate.flag = true;
+			options.translate.vector = reshape(varargin{arg_ind},[3 1]);
+		case {'axis','rotation_axis'}
+			options.rotate.flag = true;
+			options.rotate.axis = varargin{arg_ind}/norm(varargin{arg_ind});
+		case {'angle','theta','rotation_angle'}
+			options.rotate.flag = true;
+			options.rotate.angle = varargin{arg_ind};
+		case {'rotate_translate_first'}
+			options.rotate_first = varargin{arg_ind};
 	end
 	opt_ind = opt_ind + 2;
 end
