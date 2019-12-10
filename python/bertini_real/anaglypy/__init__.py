3357079d16f9cc4de38285946e2210bf11383e6d
foongminwong <foongmin@hotmail.com>
2019-12-09 18:23:20 -0600

    fix anaglypy decomposition data structure

--- python/bertini_real/anaglypy/__init__.py
+++ python/bertini_real/anaglypy/__init__.py
@@ -1,4 +1,13 @@
+"""
+Foong Min Wong
+University of Wisconsin, Eau Claire
+Summer 2019
 
+.. module:: anaglypy
+    :platform: Unix, Windows
+    :synopsis: The anagylpy module contains python and shell scripts to generate 3d anaglyph video animation of algebraic surfaces.
+
+"""
 
 if __name__ == "__main__":
     import anaglypy
