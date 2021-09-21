16a7f0064a72086a7291fdcbe12402ce83c48364
silviana amethyst <1388063+ofloveandhate@users.noreply.github.com>
2021-09-21 14:10:11 -0500

    corrections to `setup.py`
    adding missing required packages (dill, glumpy), removing glumpy from extras, adding comment on how to get glumpy / opengl working on macos big sur if one gets `ImportError: ('Unable to load OpenGL library', 'dlopen(OpenGL, 10): image not found', 'OpenGL', None)`.  see also [this issue on pyimgui](https://github.com/pyimgui/pyimgui/issues/212), which helped me get past that.

--- python/setup.py
+++ python/setup.py
@@ -2,25 +2,35 @@ from setuptools import find_packages, setup
 
 EXCLUDE_FROM_PACKAGES = []
 
-extras = {
+# as it turns out, glumpy is not in fact optional
+# because importing fails without it.
 
-    'optional': [
-        'glumpy',
-    ]
-}
+# extras = {
+
+#     'optional': [
+#         'glumpy',
+#     ]
+# }
 
 setup(name='bertini_real',
       version='1.6',  # TODO make this set programmatically
       description='Python library for bertini_real',
       url='http://bertinireal.com',
-      author='Dan Hessler, Foong Min Wong, Silviana Amethyst',
-      author_email='brakeda@uwec.edu',
+      author='Silviana Amethyst with Dan Hessler, Foong Min Wong',
+      author_email='amethyst@uwec.edu',
       packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
       install_requires=['matplotlib',
                         'trimesh',
                         'pyopengl',
                         'glfw',
-                        'triangle'],
+                        'triangle',
+                        'dill',
+                        'glumpy'],
       extras_require=extras,
       package_dir={'bertini_real': 'bertini_real'},
       zip_safe=False)
+
+# on macos, i had to patch site-packages/OpenGL/platfomr/ctypesload.py
+# as in https://githubmemory.com/repo/swistakm/pyimgui/issues/212
+# that post indicated to change line 80, but I had to change line 35.
+# it did work...  but it makes me uncomfortable
