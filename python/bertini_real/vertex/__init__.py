d77de2b1f376c9dc715da92e23739fcd16ea2f7b
silviana amethyst <1388063+ofloveandhate@users.noreply.github.com>
2022-02-02 15:25:56 -0600

    now actually printing vertexes

--- python/bertini_real/vertex/__init__.py
+++ python/bertini_real/vertex/__init__.py
@@ -11,25 +11,26 @@ class Vertex:
 
     """
 
-    def __init__(self, point, input_filename_index, projection_value, type):
+    def __init__(self, point, input_filename_index, projection_value, vertex_type):
         """ Initialize a Vertex object
 
-            :param point: directory path
+            :param point: coordinates
             :param input_filename_index
             :param projection_value: value of projection
-            :param type: vertextype
+            :param vertex_type: vertextype
         """
         self.point = point
         self.input_filename_index = input_filename_index
         self.projection_value = projection_value
-        self.type = type
+        self.type = vertex_type
+
+    def __repr__(self):
+        """ toString method for Vertex """
+        val = f"Vertex({self.point},{self.input_filename_index},{self.type})"
+        return val
 
     def __str__(self):
-        """ toString method for Vertex """
-        val = "Vertex with coordinates: " + str(self.point)
-        val += "\n\nVertex Type: {}".format(self.type)
-        # val += str(self.type)
-        return val
+        return repr(self)
 
     def is_of_type(self, type):
         """ Check if a vertex matches certain VertexType
