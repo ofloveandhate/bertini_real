147d54829b5a798c9e35bb07ca31cd783a4f18dc
silviana amethyst <1388063+ofloveandhate@users.noreply.github.com>
2022-11-19 18:19:45 -0600

    Create piece_skeleton_experiment.py

---
+++ test/surface/whitney/piece_skeleton_experiment.py
@@ -0,0 +1,41 @@
+import bertini_real as br
+
+import json
+import numpy as np
+
+# br.data.gather_and_save()
+surface = br.data.read_most_recent()
+surface_pieces = surface.separate_into_nonsingular_pieces()
+
+
+
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+                            np.int16, np.int32, np.int64, np.uint8,
+                            np.uint16, np.uint32, np.uint64)):
+            return int(obj)
+        elif isinstance(obj, (np.float_, np.float16, np.float32,
+                              np.float64)):
+            return float(obj)
+        elif isinstance(obj, (np.ndarray,)):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
+        
+
+for surface_piece in surface_pieces:
+
+    edge_pieces = surface_piece.edge_pieces()
+
+    edge_pieces_as_points = []
+    for ii,edge_piece in enumerate(edge_pieces):
+        edge_pieces_as_points.append( (edge_piece.inputfilename()+"_"+str(ii),edge_piece.to_points()) ) 
+
+    with open(surface_piece.generate_filename_no_ext(basename="touching_curves")+'.json','w') as f:
+
+        # print(edge_pieces_as_points)
+        json.dump(edge_pieces_as_points, f,cls=NumpyEncoder, indent=4)  
