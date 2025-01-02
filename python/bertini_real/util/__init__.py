eebb9d4be3dc1edab7a7e474f9bab13dc38d2b9d
silviana amethyst <1388063+ofloveandhate@users.noreply.github.com>
2025-01-02 21:44:01 +0100

    next filenumber works for any pattern (with a single *)

--- python/bertini_real/util/__init__.py
+++ python/bertini_real/util/__init__.py
@@ -5,33 +5,17 @@
       * to enhance lists
 """
 
-def next_filenumber():
+def next_filenumber(pattern='BRdata*.pkl'):
     """ Keep track on the next filenumber for BRData
 
         :rtype: An integer which is the next file number of BRData*.pkl
 
     """
-    import fnmatch
-    import os
 
-    pattern = 'BRdata*.pkl'
-
-    files = os.listdir('.')
-    highest_number = -1
-
-    for name in files:
-        if fnmatch.fnmatch(name, pattern):
-            try:
-                current_number = int(name[6:-4])
-                if current_number > highest_number:
-                    highest_number = current_number
-            except ValueError:
-                continue
-
-    return highest_filenumber()+1
+    return highest_filenumber(pattern)+1
 
 
-def highest_filenumber():
+def highest_filenumber(pattern = 'BRdata*.pkl'):
     """ Get the highest/most recent file number of BRData
 
         :rtype: The highest integer of BRData*.pkl
@@ -39,16 +23,15 @@ def highest_filenumber():
     """
     import fnmatch
     import os
-
-    pattern = 'BRdata*.pkl'
-
+    a,b = pattern.split('*')
+    
     files = os.listdir('.')
     highest_number = -1
 
     for name in files:
         if fnmatch.fnmatch(name, pattern):
             try:
-                current_number = int(name[6:-4])
+                current_number = int(name[len(a):-len(b)])
                 if current_number > highest_number:
                     highest_number = current_number
             except ValueError:
